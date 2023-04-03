6772f897e0527a6935abae8153cb42f7e3f2a2a8
Ulla Aeschbacher <ulla.aeschbacher@bsi-software.com>
2023-04-03 09:39:10 +0200

    Day 12

---
+++ src/day12/Main.java
@@ -0,0 +1,36 @@
+package day12;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        File inputFile = new File("./src/day7/input.txt");
+        //File inputFile = new File("./src/day7/example2.txt");
+        Scanner scanner = new Scanner(inputFile).useDelimiter("\r\n");
+        List<String> input = new ArrayList<>();
+        String currentLine;
+        while (scanner.hasNext()) {
+            currentLine = scanner.nextLine();
+
+        }
+        scanner.close();
+        int result1 = solve1(input);
+        System.out.println("Result 1:" + result1);
+        int result2 = solve2(input);
+        System.out.println("Result 2:" + result2);
+    }
+
+    private static int solve1(List<String> input) {
+        return input.size();
+    }
+
+    private static int solve2(List<String> input) {
+        return input.size();
+    }
+
+}
